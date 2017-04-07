b4cf285b9e1d3e1990039ccf2c0383ce91b28ee9
Tonnytg <tonnytg@archeiro.tonnytg.com.br>
2017-04-06 22:15:11 -0300

    Resultado para casas de 1 digito

--- game-Roman Numerals/solution.py
+++ game-Roman Numerals/solution.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+ROMAN = ['0','I','II','III','IV','V','VI','VII','VIII','IX','X']
+
 print ("Iniciando solução para jogo de converter número inteiro para Romano")
 number = input("Insira o numero")
-print (number)
+
+if ( len(number) == 1 ):
+    print (ROMAN[int(number)] )   
