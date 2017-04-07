020f9781ce4aa5973ef7f4c7d98c1d52eb8c3f8d
Tonnytg <tonnytg@archeiro.tonnytg.com.br>
2017-04-06 23:11:30 -0300

    Simples code convert finish

--- game-Roman Numerals/solution.py
+++ game-Roman Numerals/solution.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 
-ROMAN = ['0','I','II','III','IV','V','VI','VII','VIII','IX','X']
+ROMAN = ['0','I','II','III','IV','V','VI','VII','VIII','IX']
+ROMANBig = [ 'X','L','C','D','M' ]
+
 number = input("Insert a number: ")
 
 if ( len(number) == 1 ):
-    print (ROMAN[int(number)] )   
+    print (ROMAN[int(number)] )
+elif ( len(number) == 2 ):
+    if ( int(number[1:2]) == 0 ):
+        print ('X')
+    else:
+        print('X' + ROMAN[int(number[1:2])]  )
