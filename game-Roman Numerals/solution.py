8f85af441432b45ed2e9994b5fc89b35685a3f0f
Tonnytg <tonnytg@archeiro.tonnytg.com.br>
2017-04-06 22:20:03 -0300

    Change a Language

--- game-Roman Numerals/solution.py
+++ game-Roman Numerals/solution.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 
 ROMAN = ['0','I','II','III','IV','V','VI','VII','VIII','IX','X']
-
-print ("Iniciando solução para jogo de converter número inteiro para Romano")
-number = input("Insira o numero")
+number = input("Insert a number: ")
 
 if ( len(number) == 1 ):
     print (ROMAN[int(number)] )   
