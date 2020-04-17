efe6b7e9519df3da5cc32a42a9af80bf4a4d680a
Antonio Thomacelli <tonnytg@gmail.com>
2020-04-16 21:19:40 -0300

    first solution test

--- long-repeate/main.py
+++ long-repeate/main.py
@@ -1,14 +1,15 @@
 def long_repeat(line):
-    """
-        length the longest substring that consists of the same char
-    """
-    # your code here
-    X = line[0]
-    for letter in line:
-        X = letter
+    try:
+        I = 0
+        for x in range (0,len(line)):
+            word = line[x+1]
+            if word == line[x]:
+                print(word)
+                I = I + 1
+        print(I)
+    except IndexError:
+        print(I)
 
-    return 0
 
 if __name__ == '__main__':
-    # These "asserts" using only for self-checking and not necessary for auto-testing
-    assert long_repeat('sdsffffse') == 4, "First"
+    long_repeat('sdsffffse')# == 4, "First"
