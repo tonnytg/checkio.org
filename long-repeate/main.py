0b8c0b9d38f0e75d7cc0e0144427bae96e6118af
Antonio Thomacelli <tonnytg@gmail.com>
2020-04-19 18:34:32 -0300

    Solução long repeat v1

--- long-repeate/main.py
+++ long-repeate/main.py
@@ -1,15 +1,24 @@
 def long_repeat(line):
     try:
-        I = 0
-        for x in range (0,len(line)):
-            word = line[x+1]
-            if word == line[x]:
-                print(word)
-                I = I + 1
-        print(I)
+        if line == "":
+            return(0)
+        numb = 1
+        num_old = 1
+        num_new = 1
+        for I in range (len(line)):
+            J = I + 1
+            if line[I] == line[J]:
+                num_new = num_new + 1
+            else:
+                num_new = 1
+            if num_new > num_old:
+                num_old = num_new
+        return(num_old)
     except IndexError:
-        print(I)
+        return(num_old)
 
 
 if __name__ == '__main__':
-    long_repeat('sdsffffse')# == 4, "First"
+    long_repeat('sdsffffse')
+    long_repeat('ddvvrwwwrggg')
+    #long_repeat('abababaab')
