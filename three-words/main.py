5a4e3376cfcc85ce17021de658280a9899e7666b
Antonio Thomacelli <tonnytg@gmail.com>
2020-04-19 21:37:04 -0300

    Three words check with sequence

--- three-words/main.py
+++ three-words/main.py
@@ -1,21 +1,35 @@
 def checkio(words):
     list = (words.split())
-    if len(list) >= 3:
-        status = True
-        for word in list:
-            try:
-                print("Inteiro:",int(word))
-                status = False
-            except ValueError:
-                if status != False:
-                    status = True
-    else:
-        status = False
-    print(status)
+    seq = 1
+    for word in list:
+        try:
+            if int(word):
+                print("Inteiro")
+                if seq <= 2:
+                    seq = 1
+        except ValueError:
+            print("String")
+            seq +=1
+    if seq >= 3:
+        return(True)
+    else
+        return(False)
+
+
+#         try:
+    #             print("Inteiro:",int(word))
+    #             status = False
+    #         except ValueError:
+    #             if status != False:
+    #                 status = True
+    #                 seq = seq + 1
+    # else:
+    #     status = False
+    # print(status)
 
 # These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
     #print('Example:')
     #print(checkio("Hello World hello"))
-    checkio("Hello 31 hello")# == True, "Hello"
-    checkio("Teste ola como vai")
+    checkio("Hello World 21 hello")
+    #checkio("Teste ola como vai")
