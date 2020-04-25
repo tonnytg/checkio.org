cf043c5e058f7b7499029dddeec51cf3afa2d7d7
Antonio Thomacelli <tonnytg@gmail.com>
2020-04-25 10:28:59 -0300

    fix a new space problem and exist two or more space behind words, with split(' ')

--- backward-each-word/main.py
+++ backward-each-word/main.py
@@ -7,7 +7,7 @@ def backward_string_by_word(text: str) -> str:
     #     word = word + (text[i])
     # print(word)
     words = ''
-    for word in text.split():
+    for word in text.split(' '):
         if words == '':
             words = word[::-1]
         elif words != '':
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     # These "asserts" are used for self-checking and not for an auto-testing
     #backward_string_by_word('abc')
     #backward_string_by_word('world') == 'dlrow'
-    backward_string_by_word('hello world') == 'olleh dlrow'
-    #backward_string_by_word('hello   world') == 'olleh   dlrow'
+    #backward_string_by_word('hello world') == 'olleh dlrow'
+    backward_string_by_word('hello   world') == 'olleh   dlrow'
     #backward_string_by_word('welcome to a game') == 'emoclew ot a emag'
     # print("Coding complete? Click 'Check' to earn cool rewards!")
