99baf1291744333319515124aa341da4fc2027fc
Antonio Thomacelli <tonnytg@gmail.com>
2020-04-25 19:02:35 -0300

    organize code and clean

--- bigger price/main.py
+++ bigger price/main.py
@@ -1,20 +1,17 @@
 def bigger_price(limit: int, data: list) -> list:
+    #https://py.checkio.org/mission/bigger-price
     data2 = []
     lista = []
     for i in range(len(data)):
         lista.insert(0,data[i]["price"])
         lista.sort(reverse=True)
-    print(lista[0])
-    print(data[1]["price"])
 
     for h in range(limit):
         for j in range(len(data)):
             if lista[h] == data[j]["price"]:
-                print("positivo")
-                print(data[j])
                 data2.append(data[j])
 
-    print("Valor final:", data2[:limit])
+    print(data2[:limit])
 
 
 if __name__ == '__main__':
