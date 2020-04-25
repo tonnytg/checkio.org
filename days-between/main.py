e9a65635f3c44663d9ea64097c623d98f0398167
Antonio Thomacelli <tonnytg@gmail.com>
2020-04-24 23:57:24 -0300

    solved problemm when value return 0:00:00

--- days-between/main.py
+++ days-between/main.py
@@ -9,7 +9,10 @@ def days_diff(A, B):
     #class datetime.datetime
     #A combination of a date and a time. Attributes: year, month, day, hour, minute, second, microsecond, and tzinfo.
     value = str((datetime.datetime( A[0],A[1],A[2] ) - datetime.datetime( B[0],B[1],B[2] ))).split(' ')
-    print(abs(int(value[0])))
+    try:
+        print(abs(int(value[0])))
+    except ValueError:
+        print(0)
 
 
 
@@ -21,3 +24,4 @@ if __name__ == '__main__':
     #days_diff((2014, 1, 1), (2014, 8, 27)) == 238
     #days_diff((2014, 8, 27), (2014, 1, 1)) == 2381982, 4, 22
     #print("Coding complete? Click 'Check' to earn cool rewards!")1982, 4, 22
+    days_diff((2014, 2, 28), (2014, 2, 28))
